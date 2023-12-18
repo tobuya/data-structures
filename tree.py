9d26ecf7ba2b22f999ce3c9d1c7e162c9da264ac
tobuya <thomasobuya95@gmail.com>
2023-12-18 22:03:06 +0300

    Create a basic visualization of a tree data structure

---
+++ tree.py
@@ -0,0 +1,22 @@
+"""The below modules will help create a basic visual representation of a tree."""
+import networkx as nx
+import matplotlib.pyplot as plt
+
+# Create a basic tree structure
+tree = nx.DiGraph()
+
+# Add nodes to the tree
+tree.add_nodes_from([1, 2, 3, 4, 5, 6, 7])
+
+# Add edges between nodes to form a tree structure
+edges = [(1, 2), (1, 3), (2, 4), (2, 5), (3, 6), (3, 7)]
+tree.add_edges_from(edges)
+
+# Visualize the tree
+pos = nx.spring_layout(tree)  # Positions of nodes in the layout
+
+plt.figure(figsize=(8, 6))
+nx.draw(tree, pos, with_labels=True, node_size=500, node_color='lightblue', font_weight='bold', font_size=12, arrows=True)
+plt.title('Basic Tree Visualization')
+plt.axis('off')  # Turn off axis labels
+plt.show()
